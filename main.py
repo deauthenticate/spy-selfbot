50758a433b829c60ec6ed72a501751a2ddf0118e
RisinPlayZ <60840691+RisinPlayZ@users.noreply.github.com>
2021-05-06 18:57:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -331,18 +331,6 @@ async def destroy(ctx):
 
 format = '%a, %d %b %Y | %H:%M:%S %ZGMT'
 
-@client.command()
-@commands.guild_only()
-async def doxserver(ctx):
-    embed = discord.Embed()
-    text_channels = len(ctx.guild.text_channels)
-    voice_channels = len(ctx.guild.voice_channels)
-    categories = len(ctx.guild.categories)
-    channels = text_channels + voice_channels
-    embed.set_thumbnail(url=(str(ctx.guild.icon_url)))
-    embed.add_field(name=f"Information About **{ctx.guild.name}**: ", value=f":white_small_square: ID: **{ctx.guild.id}** \n:white_small_square: Owner: **{ctx.guild.owner}** \n:white_small_square: Location: **{ctx.guild.region}** \n:white_small_square: Creation: **{ctx.guild.created_at.strftime(format)}** \n:white_small_square: Members: **{ctx.guild.member_count}** \n:white_small_square: Channels: **{channels}** Channels; **{text_channels}** Text, **{voice_channels}** Voice, **{categories}** Categories \n:white_small_square: Verification: **{str(ctx.guild.verification_level).upper()}** \n:white_small_square: Features: {', '.join((**{x}** for x in ctx.guild.features))} \n:white_small_square: Splash: {ctx.guild.splash}")
-    embed.set_footer(text='Created by RisinPlayZ')
-    await ctx.send(embed=embed)
 
 
 @client.command(aliases=['deletewebhook'])
